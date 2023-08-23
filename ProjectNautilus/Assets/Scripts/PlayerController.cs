b99a439bf3bc45d2c2586efc63edec6c59ca8a4e
samuelmoores <samueldmoores@gmail.com>
2023-08-22 21:03:49 -0700

    Player rotation and swimming animation implemented

--- ProjectNautilus/Assets/Scripts/PlayerController.cs
+++ ProjectNautilus/Assets/Scripts/PlayerController.cs
@@ -20,23 +20,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        rotation.x = Input.GetAxis("Horizontal");
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
-        rotation.x = Input.GetAxis("Horizontal");
+        movement.Normalize();
 
         rb.AddForce(movement * swimSpeed, ForceMode.Impulse);
 
-        if(rotation != Vector2.zero)
+        if (rotation != Vector2.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(rotation, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
 
+        }
+
+        if(movement != Vector2.zero)
+        {
+            animator.SetBool("isSwimming", true);
         }else
         {
-            //animator.SetBool("isSwimming", false);
-
+            animator.SetBool("isSwimming", false);
         }
+        
 
     }
 }
